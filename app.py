77d6baeb8a7b25d4c8a334e89fdba78c87890638
ALI84CC <ALISON84CC@GMAIL.COM>
2023-02-15 00:02:17 +0000

    atualizações do bloc em python

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
-from flask import Flask
+from flask import Flask,render_template
 
-app = Flask("Hello")
+app = Flask(__name__)
+#POSTS MOC 
+
+posts =[
+    {
+        "titulo" : "Post 1",
+        "texto": "Meu primeiro post"
+    },
+    {
+        "titulo" : "Post 2",
+        "texto": "Meu segundo post"
+    },
+    {
+        "titulo" : "Post 3",
+        "texto": "Meu terceiro post"
+    },
+]
 
 @app.route('/')
-def Hello():
-    return"<h1>Hello World</h1>"
+def exibir_entradas():
+    return render_template("exibir_entradas.html", entradas=posts)
+
+   
